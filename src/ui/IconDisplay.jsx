f8161846ef695e696efbddf8cdef03cc6946e332
GAURAV SINHA <sinhagaurav.me@gmail.com>
2025-07-01 14:36:42 +0530

    fix: adjust sparkle icon size

--- src/ui/IconDisplay.jsx
+++ src/ui/IconDisplay.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 const IconDisplay = () => (
     <motion.svg
         viewBox="0 0 170 170"
-        className="w-24 h-24 mb-[2px] ml-[-1rem]"
+        className="w-28 h-28 mb-[2px] ml-[-1rem]"
         initial={{ opacity: 0, x: -80, scale: 0.2, rotate: -180 }}
         animate={{ opacity: 1, x: 0, scale: 1, rotate: 0 }}
         transition={{
